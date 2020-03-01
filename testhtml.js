ce677cc0104228f55d2a51534ad1ce3527089329
Guillermo López García <jdkdejava@gmail.com>
2020-03-01 15:21:38 +0100

    automatic test maked in GitHub actions

--- testhtml.js
+++ testhtml.js
@@ -6,5 +6,15 @@ for(const f of files)
 {
     validate('src/' + f + '.html').then(result => {
         console.log(result);
+        return;
     });
 }
+// validate('src/index.html').then(result => {
+//     for(const m of result.messages)
+//     {
+//         if(m.type === 'error')
+//         {
+//             console.log(m)
+//         }
+//     }
+// });
