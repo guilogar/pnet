496f1719916b368c9c7c4ba11e88bd2c2dc5ca79
Guillermo López García <guillermo.lopezgarcia@alum.uca.es>
2020-05-23 19:19:08 +0200

    widget finished

---
+++ android_application/app/src/main/java/es/uca/android_application/Widget/WidgetProvider.java
@@ -0,0 +1,13 @@
+package es.uca.android_application.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WidgetProvider extends AppWidgetProvider
+{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+    }
+}
